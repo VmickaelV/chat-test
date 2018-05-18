3265c17a27fdc636805dfc9273ba8b1b44e40836
Michael VIEGAS <michael.viegas.ext@veolia.com>
2018-05-18 15:08:38 +0200

    Déplacer le code source vers un module serveur

--- src/main/kotlin/com/excilys/mviegas/chattest/dao/AccountDao.kt
+++
@@ -1,7 +0,0 @@
-package com.excilys.mviegas.chattest.dao
-
-import com.excilys.mviegas.chattest.model.Account
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.security.core.userdetails.UserDetailsService
-
-interface AccountDao : PagingAndSortingRepository<Account, String>
