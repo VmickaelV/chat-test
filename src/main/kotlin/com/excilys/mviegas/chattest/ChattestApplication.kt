7e5c52a17e0bc6c306999b07348b8d3ccac42854
mviegas <mviegas@excilys.com>
2021-09-01 01:26:06 +0200

    Re init Project

---
+++ src/main/kotlin/com/excilys/mviegas/chattest/ChattestApplication.kt
@@ -0,0 +1,11 @@
+package com.excilys.mviegas.chattest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChattestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChattestApplication>(*args)
+}
---
+++ src/test/kotlin/com/excilys/mviegas/chattest/ChattestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.excilys.mviegas.chattest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChattestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
